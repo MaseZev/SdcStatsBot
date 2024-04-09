c43cff807d04b53b8daeeeaf7253bf73fa4a3759
MaseZev <63608578+MaseZev@users.noreply.github.com>
2024-04-09 16:45:00 +0500

    Update client.py

--- SdcStatsBot/client.py
+++ SdcStatsBot/client.py
@@ -1,17 +1,18 @@
 import aiohttp
 import asyncio
-import logging
-from .interval import Global
 
 class SdcApi:
-    def __init__(self, logger=None, interval=30):
+    def __init__(self, interval=30):
+        if not isinstance(interval, int):
+            raise ValueError("interval must be an integer")
+        if interval < 30:
+            raise ValueError("interval must be a positive integer")
+
         self.web_url = "https://api.server-discord.com/v2"
         self.session = aiohttp.ClientSession()
-        self.logger = logger or logging.getLogger(__name__)
         self.interval = interval
-        Global.interval = interval
 
-    async def post_stats_loop(self, bot_id, sdc_token, servers_count, shard_count):
+    async def post_stats_loop(self, bot_id: int, sdc_token: str, servers_count: int, shard_count: int):
         while True:
             await self.post_stats(bot_id, sdc_token, servers_count, shard_count)
             await asyncio.sleep(self.interval * 60)
@@ -31,12 +32,16 @@ class SdcApi:
             url = f"{self.web_url}/bots/{bot_id}/stats"
             payload = {"shards": shard_count, "servers": servers_count}
             async with self.session.post(url, headers=headers, json=payload) as response:
-                if response.status != 200:
-                    self.logger.error("Failed to send statistics to SDC API: %s", await response.text())
-                else:
-                    self.logger.info("Statistics successfully sent to SDC API. Next update in %s minutes", self.interval)
+                data = await response.json()
+                try:
+                    error = data['error']
+                except:
+                    error = None
+                if not error:
+                    return print("Statistics successfully sent to SDC API. Next update in %s minutes", self.interval)
+                print("Failed to send statistics to SDC API: %s", data['error']['message'])
         except aiohttp.ClientError as e:
-            self.logger.error("Error while sending statistics to SDC API")
+            print("Error while sending statistics to SDC API")
 
-    async def start_posting_stats(self, bot_id, sdc_token, servers_count, shard_count=1):
+    async def start_posting_stats(self, bot_id: int, sdc_token: str, servers_count: int, shard_count: int = 1):
         asyncio.create_task(self.post_stats_loop(bot_id, sdc_token, servers_count, shard_count))

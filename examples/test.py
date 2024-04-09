40c6bae1522d94807d0ddd369d346145e95f82a1
MaseZev <63608578+MaseZev@users.noreply.github.com>
2024-04-09 16:46:10 +0500

    Add files via upload

--- examples/test.py
+++ examples/test.py
@@ -1,20 +1,18 @@
-from SDcStatsBot import SdcApi
+from SdcStatsBot import SdcApi
 import disnake
 from disnake.ext import commands
-import logging
 
-logging.basicConfig(level=logging.INFO)
 bot = commands.Bot(command_prefix="!", intents=disnake.Intents.all())
 
 
 async def main():
-    api = SdcApi(logger=logging.getLogger(__name__), interval=60) #Update interval 60 minutes
-    await api.start_posting_stats(bot_id=1234567890, # Bot Id
-                                  sdc_token="token_sdc_api", # Token from the Sdc website
-                                  servers_count=1000, shard_count=1) # It is not necessary to set the number of shards
+    api = SdcApi(interval=60)
+    await api.start_posting_stats(bot_id=1234567890,
+                                  sdc_token="token_sdc_api",
+                                  servers_count=439, shard_count=1)
 
 @bot.event
 async def on_ready():
     await main()
 
-bot.run("token bot") # Token bot discord developer portal
+bot.run("toke bot")
